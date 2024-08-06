4cbb8992f3a3aa0836a6b4cb4c768715a23464e8
Wikho <wikho.doman@gmail.com>
2024-08-06 18:20:27 +0200

    Currency Manager

---
+++ Assets/_Scripts/Managers/CurrencyManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+//Please Read KAT!
+//This script main purpoes is to handle currency transactions and operations, the script is a Singleton and can be called in any script by using Instance.CurrencyManager.(function/procedure) 
+//Important (1-3)
+//-1- Use the Buy function to remove curreny
+//-2- Use the GetCurrency for UI
+//-3- Use AddCurrency for adding 
+
+public class CurrencyManager : MonoBehaviour
+{
+    #region Variables
+    //Singleton
+    public static CurrencyManager Instance;
+
+    //Currency
+    [Header("Currency")]
+    [SerializeField] private float currency = 0;
+
+    #endregion
+
+    private void Awake()
+    {
+        Singleton();
+    }
+
+    #region Functions
+
+    public bool Buy(float price)
+    {
+        if(currency>= price)//Enough money
+        {
+            RemoveCurrency(price);
+            return true;
+        }
+        else
+        { 
+            return false;
+        }
+    }
+
+    #endregion
+
+    #region Get & Set & Add
+    //Followers
+    public float GetCurrency() { return currency; }
+    private void SetCurrency(float value) { currency = value; }
+    public void AddCurrency(float currencyAdded) { currency += currencyAdded; }
+    private void RemoveCurrency(float currencyRemoved) { currency -= currencyRemoved; }
+    #endregion
+
+    #region Singleton
+
+    private void Singleton()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            Debug.LogWarning("Another instance of GameLobbyManager was destroyed on creation!");
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    #endregion
+}

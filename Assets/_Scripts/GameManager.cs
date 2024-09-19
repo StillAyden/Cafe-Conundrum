a83fd29839756c0bf9a26a79d428c73e6d61e63c
StillAyden <aydenshaw14@gmail.com>
2024-09-19 20:19:01 +0200

    HouseKeeping
    - Sorted Files and Folders.
    - Removed TMP (for now).
    - Prepped for main push.

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Debug.unityLogger.logEnabled = false;                 //Uncomment to disable all "Debug.Log()"
+    }
+}

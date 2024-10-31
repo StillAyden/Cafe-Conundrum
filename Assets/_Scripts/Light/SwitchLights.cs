b88011d5bb1aeaf05060f3608247f6ec1ca654e4
Wikho <wikho.doman@gmail.com>
2024-10-31 20:38:49 +0200

    Lights and Textures & Models

---
+++ Assets/_Scripts/Light/SwitchLights.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLights : MonoBehaviour
+{
+    public static SwitchLights Instance;
+
+    public Texture2D[] darkLightmapDir, darkLightmapColor;
+
+    public Texture2D[] brightLightmapDir, brightLightmapColor;
+
+    private LightmapData[] darkLightmap, brightLightmap;
+
+    public bool lightsOn = true;
+
+    private void Start()
+    {
+        Instance = this;
+
+        //Dark LightMap
+        List<LightmapData> dlightmap = new List<LightmapData>();
+
+        for (int i = 0; i < darkLightmapDir.Length; i++)
+        {
+            LightmapData lmData = new LightmapData();
+
+            lmData.lightmapDir = darkLightmapDir[i];
+            lmData.lightmapColor = darkLightmapColor[i];
+        
+            dlightmap.Add(lmData);
+        }
+
+        darkLightmap = dlightmap.ToArray();
+
+
+        //Bright LightMap
+        List<LightmapData> blightmap = new List<LightmapData>();
+
+        for (int i = 0; i < brightLightmapDir.Length; i++)
+        {
+            LightmapData lmData = new LightmapData();
+
+            lmData.lightmapDir = brightLightmapDir[i];
+            lmData.lightmapColor = brightLightmapColor[i];
+
+            blightmap.Add(lmData);
+        }
+
+        brightLightmap = blightmap.ToArray();
+
+    }
+
+    private void OnValidate()
+    {
+        if(lightsOn)
+        {
+            LightsOn();
+        }
+        else
+        {
+            LightsOff();
+        }
+    }
+
+    public void LightsOn()
+    {
+        LightmapSettings.lightmaps = brightLightmap;
+    }
+
+    public void LightsOff()
+    {
+        LightmapSettings.lightmaps = darkLightmap;
+    }
+}
